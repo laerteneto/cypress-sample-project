86d1d36f923a689710cc463392d919a28ca8a5e6
Laerte Mello <LMello@globalshares.com>
2021-02-11 16:18:15 +0000

    adding new file for snapthot

--- cypress/support/pages/basePage.js
+++ cypress/support/pages/basePage.js
@@ -6,7 +6,7 @@ class BasePage {
         createDiffImage: true, // Should a "diff image" be created, can be disabled for performance
         threshold: 0.1, // Amount in pixels or percentage before snapshot image is invalid
         thresholdType: "percent", // Can be either "pixel" or "percent",
-        resizeDevicePixelRatio: false // Resize image to base resolution when Cypress is running on high DPI screen, `cypress run` always runs on base resolution
+        resizeDevicePixelRatio: true // Resize image to base resolution when Cypress is running on high DPI screen, `cypress run` always runs on base resolution
       },
       separator: " ", // Naming resulting image file with a custom separator rather than using the default ` #`
     });
